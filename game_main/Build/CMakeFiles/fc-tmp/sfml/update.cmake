772acdf82e54cef2b92646776e956aac632c0114
曹越 <3518231995@qq.com>
2026-02-01 14:08:00 +0800

    .2

--- game_main/Build/CMakeFiles/fc-tmp/sfml/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for sfml")
-
-block(SCOPE_FOR VARIABLES)
-
-include("D:/elab/game_study_team/RunawayDUT353/RunawayDUT353/game_main/Build/CMakeFiles/fc-tmp/sfml/sfml-gitupdate.cmake")
-
-endblock()
--- game_main/Build/_deps/flac-build/src/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/elab/game_study_team/RunawayDUT353/RunawayDUT353/game_main/Build/_deps/flac-src/src
-# Build directory: D:/elab/game_study_team/RunawayDUT353/RunawayDUT353/game_main/Build/_deps/flac-build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libFLAC")
